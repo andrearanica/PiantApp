79c81171c32e00cf0bda1ed9cabaf4eda0eb1ca5
andrearanica <andrearanica2004@gmail.com>
2022-10-31 15:12:45 +0100

    🔎 First home

---
+++ GrouPX-Client/GrouPX-Client/Account.cs
@@ -0,0 +1,8 @@
+﻿namespace GrouPX_Client {
+    class Account {
+        public bool logged { get; set; }
+        public Account () {
+            logged = false;
+        }
+    }
+}
--- GrouPX-Client/GrouPX-Client/Form1.Designer.cs
+++ GrouPX-Client/GrouPX-Client/Form1.Designer.cs
@@ -29,13 +29,83 @@ namespace GrouPX_Client
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_account = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.btn_account);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(96, 516);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_account
+            // 
+            this.btn_account.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btn_account.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_account.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btn_account.Image = ((System.Drawing.Image)(resources.GetObject("btn_account.Image")));
+            this.btn_account.Location = new System.Drawing.Point(3, 27);
+            this.btn_account.Name = "btn_account";
+            this.btn_account.Size = new System.Drawing.Size(88, 89);
+            this.btn_account.TabIndex = 0;
+            this.btn_account.UseVisualStyleBackColor = false;
+            this.btn_account.Click += new System.EventHandler(this.btn_account_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(101, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "GrouPX";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(247, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lo sport va condiviso";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 510);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_account;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
